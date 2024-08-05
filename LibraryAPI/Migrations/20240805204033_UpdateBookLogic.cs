d2643e0704aa72e990a895a47f6a7d70511e8e00
Kirill <krvchnkkrll@gmail.com>
2024-08-05 23:41:25 +0300

    Add migration

---
+++ LibraryAPI/Migrations/20240805204033_UpdateBookLogic.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookLogic : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BorrowPeriod",
+                table: "UserBooks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BorrowPeriod",
+                table: "UserBooks");
+        }
+    }
+}
